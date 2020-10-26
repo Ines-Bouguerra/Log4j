7a7108fc2174d4c39d002cd84f7c3ddd9d725ab7
Ines-Bouguerra <ines.bouguerra2207@gmail.com>
2020-10-26 10:53:37 +0100

    Log4j advanced log

--- src/main/java/tn/esprit/AffichageLog.java
+++ src/main/java/tn/esprit/AffichageLog.java
@@ -13,14 +13,13 @@ public class AffichageLog {
 
 	public void getAllPrducts() {
 		try {
-			l.info("In getAllPrducts() : ");
+			l.trace("In getAllPrducts() : ");
 			l.debug("Je vais lancer la divsion.");
 			int i = 1 / 0;
 			l.debug("Je viens de lancer la divsion. " + i);
 			l.debug("Je viens de finir l'opération X.");
 			l.info("Out getAllPrducts() without errors.");
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			l.error("Erreur dans getAllPrducts() : " + e);
 		}
 	}
